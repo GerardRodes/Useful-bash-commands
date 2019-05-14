f8715eeefebf75d13e244ff62a8ad9b9caffd80e
Gerard Rodes <gerard.rodes@omniaccess.com>
2019-05-14 14:17:13 +0200

    added chrome no cors

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,6 @@
-
 alias update-vscode="wget https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable -O /tmp/code_latest_amd64.deb && sudo dpkg -i /tmp/code_latest_amd64.deb"
 alias j="jump"
 alias tcli="/home/grodes/devel/td-360-cli/main.sh"
 alias gcd="git checkout development"
 alias pitbull="echo \"1, 2, 3 dale\" && git pull"
+alias chrome-no-cors="google-chrome --disable-web-security --user-data-dir="$HOME/.config/google-chrome" --allow-file-access-from-files > /dev/null 2>&1 &"
